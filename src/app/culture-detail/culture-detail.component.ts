c8e62fd9d8a9a61b3ab03ea7cafe77ba81177947
light0617 <light11470617@gmail.com>
2018-08-15 22:16:12 -0700

    finish

--- src/app/culture-detail/culture-detail.component.ts
+++ src/app/culture-detail/culture-detail.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { Culture } from '../shared/culture';
+import { CultureService } from '../services/culture.service';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-culture-detail',
   templateUrl: './culture-detail.component.html',
-  styleUrls: ['./culture-detail.component.scss']
+  styleUrls: ['./culture-detail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    visibility(),
+    flyInOut(),
+    expand()
+  ]
 })
 export class CultureDetailComponent implements OnInit {
 
-  constructor() { }
+  culture: Culture;
+  culturecopy = null;
+  cultureIds: string[];
+  prev: string;
+  next: string;
+  visibility = 'shown';
+  errMess: string;
+  isLike: boolean;
+  constructor(private cultureservice: CultureService,
+              private route: ActivatedRoute,
+              private location: Location,
+              @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
+    this.isLike = false;
+
+    this.cultureservice.getCultureIds()
+        .subscribe(cultureIds => this.cultureIds = cultureIds);
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.cultureservice.getCulture(params['id']); }))
+                   .subscribe(culture => { this.culture = culture; this.culturecopy = culture; this.setPrevNext(culture._id); this.visibility = 'shown'; },
+                     errmess => this.errMess = <any>errmess);
+  }
+
+  setPrevNext(cultureId: string){
+    const index = this.cultureIds.indexOf(cultureId);
+    this.prev = this.cultureIds[(this.cultureIds.length + index - 1) % this.cultureIds.length];
+    this.next = this.cultureIds[(this.cultureIds.length + index + 1) % this.cultureIds.length];
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  changeColor(tabclicked): void {
+    this.isLike= !this.isLike;
+  }
+
+  changeStyle(tabclicked): void {
+    this.isLike= !this.isLike;
   }
 
 }
--- src/app/mountain-detail/mountain-detail.component.ts
+++ src/app/mountain-detail/mountain-detail.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { Mountain } from '../shared/mountain';
+import { MountainService } from '../services/mountain.service';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-mountain-detail',
   templateUrl: './mountain-detail.component.html',
-  styleUrls: ['./mountain-detail.component.scss']
+  styleUrls: ['./mountain-detail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    visibility(),
+    flyInOut(),
+    expand()
+  ]
 })
 export class MountainDetailComponent implements OnInit {
 
-  constructor() { }
+  mountain: Mountain;
+  mountaincopy = null;
+  mountainIds: string[];
+  prev: string;
+  next: string;
+  visibility = 'shown';
+  errMess: string;
+  isLike: boolean;
+  constructor(private mountainservice: MountainService,
+              private route: ActivatedRoute,
+              private location: Location,
+              @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
+    this.isLike = false;
+
+    this.mountainservice.getMountainIds()
+        .subscribe(mountainIds => this.mountainIds = mountainIds);
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.mountainservice.getMountain(params['id']); }))
+                   .subscribe(mountain => { this.mountain = mountain; this.mountaincopy = mountain; this.setPrevNext(mountain._id); this.visibility = 'shown'; },
+                     errmess => this.errMess = <any>errmess);
   }
 
+  setPrevNext(mountainId: string){
+    const index = this.mountainIds.indexOf(mountainId);
+    this.prev = this.mountainIds[(this.mountainIds.length + index - 1) % this.mountainIds.length];
+    this.next = this.mountainIds[(this.mountainIds.length + index + 1) % this.mountainIds.length];
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  changeColor(tabclicked): void {
+    this.isLike= !this.isLike;
+  }
+
+  changeStyle(tabclicked): void {
+    this.isLike= !this.isLike;
+  }
 }
