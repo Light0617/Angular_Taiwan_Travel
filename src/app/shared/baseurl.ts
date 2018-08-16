20b7c445e511daf99ce09c3aa1f1ec19e2cc4441
light0617 <light11470617@gmail.com>
2018-08-15 22:47:28 -0700

    chagne to aws

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:4000/';
-//export const baseURL = 'http://52.13.216.5:4000/';
+//export const baseURL = 'http://localhost:4000/';
+export const baseURL = 'http://52.13.216.5:4000/';
